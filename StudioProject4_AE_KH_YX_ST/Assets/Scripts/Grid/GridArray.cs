941386988131f7a40ab26fd603d69f8a29a7e627
darth343 <darth343@gmail.com>
2017-02-14 17:56:57 +0800

    Added GetGridAtPosition Code to GridArray and added SharedData Singleton

--- StudioProject4_AE_KH_YX_ST/Assets/Scripts/Grid/GridArray.cs
+++ StudioProject4_AE_KH_YX_ST/Assets/Scripts/Grid/GridArray.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GridArray : MonoBehaviour
@@ -11,6 +12,7 @@ public class GridArray : MonoBehaviour
     public int m_rows;
     public int m_columns;
     public GameObject[,] gridmesh;
+    public Text debugtext;
 
 	// Use this for initialization
 	void Start ()
@@ -18,8 +20,17 @@ public class GridArray : MonoBehaviour
         GenerateGrid();	
 	}
 
-    GameObject GetGridAtPosition(Vector3 position)
+    public GameObject GetGridAtPosition(Vector3 position)
     {
+        int index_x = (int)(position.x - GridSizeX * 0.5f) / GridSizeX;
+        int index_z = (int)(position.z - GridSizeZ * 0.5f) / GridSizeZ;
+
+        if (index_x >= 0 && index_x <= m_rows &&
+            index_z >= 0 && index_z <= m_columns)
+        { 
+            return gridmesh[index_x, index_z];
+        }
+
         return null;
     }
 
@@ -27,9 +38,9 @@ public class GridArray : MonoBehaviour
     {
         if (isGenerated)
             return;
-        int m_rows = (int)ground.terrainData.size.x / GridSizeX;
-        int m_columns = (int)ground.terrainData.size.z / GridSizeZ;
-        gridmesh = new GameObject[m_rows, m_columns]; // hardcoded, not sure if const var can be changed in editor , change this if you don't want to hardcode Arun
+        m_rows = (int)ground.terrainData.size.x / GridSizeX;
+        m_columns = (int)ground.terrainData.size.z / GridSizeZ;
+        gridmesh = new GameObject[m_rows, m_columns];
         Debug.Log("run");
         // Create rows
         for (int x = 0; x < m_rows; x++)
---
+++ StudioProject4_AE_KH_YX_ST/Assets/Scripts/SharedData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SharedData : MonoBehaviour {
+    public static SharedData instance = null;
+    public GridArray gridmesh;
+    public Text debuginfo;
+	// Use this for initialization
+	void Start ()
+    {
+	    if(instance == null)
+        {
+            instance = this;
+        }else
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
