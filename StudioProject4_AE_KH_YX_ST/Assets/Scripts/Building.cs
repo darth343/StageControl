e38bb3f10a97be807b4ebad53d04dddc2d5e83bd
Yeo Yue Xian <glah14@hotmail.com>
2017-02-20 12:28:04 +0800

    Modified and finalized snap to grid building(changed gridarray a little)

--- StudioProject4_AE_KH_YX_ST/Assets/Scripts/Building.cs
+++ StudioProject4_AE_KH_YX_ST/Assets/Scripts/Building.cs
@@ -13,14 +13,30 @@ public class Building : MonoBehaviour {
     //GameObject Unit;//the unit that this building spawns
     public float buildtime, spawntime;//time to construct the building/time it takes to spawna asingle unit
     public int size;//building size
+    public Material holo,undamage,damaged;
     BUILDSTATE b_state;
 	// Use this for initialization
 	void Start () {
+        b_state = BUILDSTATE.B_HOLOGRAM;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        //switch(b_state)
+        //{
+        //    case BUILDSTATE.B_HOLOGRAM:
+        //        gameObject.transform.GetChild(0).GetComponent<Renderer>().material = holo;
+
+        //        break;
+        //    case BUILDSTATE.B_CONSTRUCT:
+        //        gameObject.transform.GetChild(0).GetComponent<Renderer>().material = undamage;
+
+        //        break;
+
+
+        //}
 	
 	}
 }
